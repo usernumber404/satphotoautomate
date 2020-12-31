fd062a31b31757aab82db3dd06880671f78f365b
usernumber404 <72470454+usernumber404@users.noreply.github.com>
2020-12-30 18:08:35 -0600

    Create satPhotoAutomation.py

---
+++ satPhotoAutomation.py
@@ -0,0 +1,11 @@
+import os
+
+os.chdir('add file path here') #change this to the filepath that you need
+def deletefiles():
+    for files in os.listdir():
+        if files != 'DONOTDELETE':  #change filename to whatever you do not want to delete
+            os.remove(files)
+            #print(files)    #to test this code ad a pound sign in front of os.remove(files)
+                            #then remove the pound sign in front of the print statement.
+
+deletefiles()
