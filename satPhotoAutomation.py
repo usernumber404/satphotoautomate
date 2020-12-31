cdc4bcb5635d68d5a683525e8a93ab8b760e8460
usernumber404 <72470454+usernumber404@users.noreply.github.com>
2020-12-31 12:24:54 -0600

    commit on 'main

--- satPhotoAutomation.py
+++ satPhotoAutomation.py
@@ -5,7 +5,7 @@ def deletefiles():
     for files in os.listdir():
         if files != 'DONOTDELETE':  #change filename to whatever you do not want to delete
             os.remove(files)
-            #print(files)    #to test this code ad a pound sign in front of os.remove(files)
+            #print(files)    #to test this code add a pound sign in front of os.remove(files)
                             #then remove the pound sign in front of the print statement.
 
 deletefiles()
