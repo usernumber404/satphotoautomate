27df8114f3291d00124d9a8cfa7f64acc1e3190d
usernumber404 <72470454+usernumber404@users.noreply.github.com>
2021-01-28 10:30:50 -0700

    commit on main

--- satPhotoAutomation.py
+++ satPhotoAutomation.py
@@ -1,11 +1,26 @@
 import os
+import ezgmail
+import shutil
 
-os.chdir('add file path here') #change this to the filepath that you need
-def deletefiles():
+def main():
+    movefilesintofolder()
+    sendEmail()
+    deleteleftovers()
+
+def sendEmail():
+    os.chdir(r'') #path to the folder python script is in
+    ezgmail.send('','','', [''])
+    #'email', 'subject', 'body',['pathtozip']
+
+def movefilesintofolder():
+    os.chdir('')  # change this to the filepath that you need
+    os.mkdir('') #add path to folder you want to create
     for files in os.listdir():
-        if files != 'DONOTDELETE':  #change filename to whatever you do not want to delete
-            os.remove(files)
-            #print(files)    #to test this code add a pound sign in front of os.remove(files)
-                            #then remove the pound sign in front of the print statement.
-
-deletefiles()
+        if files != '':  #change filename to whatever you do not want to delete
+            shutil.move(files,'') #add path to folder made on line 18
+    shutil.make_archive('','zip','')
+    #'path to folder on line 18', 'zip', 'path to folder on line 18'
+def deleteleftovers():
+    os.remove('') #path to zip folder created on line 22
+    shutil.rmtree('') #path to folder created on line 18
+main()
