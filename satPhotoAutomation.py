3cf045544ddbb1006bf181fb28683b4609b3f93e
usernumber404 <72470454+usernumber404@users.noreply.github.com>
2021-01-31 13:53:24 -0700

    commit on main

--- satPhotoAutomation.py
+++ satPhotoAutomation.py
@@ -1,26 +1,29 @@
 import os
-import ezgmail
 import shutil
+import datetime
 
-def main():
-    movefilesintofolder()
-    sendEmail()
-    deleteleftovers()
 
-def sendEmail():
-    os.chdir(r'') #path to the folder python script is in
-    ezgmail.send('','','', [''])
-    #'email', 'subject', 'body',['pathtozip']
+projectfolder = '/Users/jaredparker1/Desktop/testfolder'
+pathtodrive = '/Users/jaredparker1/Google Drive/satphotos/'
+currentdateandtime = datetime.datetime.now()
+currentdateandtimestring = str(currentdateandtime)
+perminatefolder = 'projections'
+newfolder = pathtodrive + currentdateandtimestring
+
 
 def movefilesintofolder():
-    os.chdir('')  # change this to the filepath that you need
-    os.mkdir('') #add path to folder you want to create
+    os.chdir(pathtodrive)
+    os.mkdir(newfolder)
+    os.chdir(projectfolder)
     for files in os.listdir():
-        if files != '':  #change filename to whatever you do not want to delete
-            shutil.move(files,'') #add path to folder made on line 18
-    shutil.make_archive('','zip','')
-    #'path to folder on line 18', 'zip', 'path to folder on line 18'
-def deleteleftovers():
-    os.remove('') #path to zip folder created on line 22
-    shutil.rmtree('') #path to folder created on line 18
-main()
+            if files != perminatefolder:
+                shutil.move(files, newfolder)
+
+
+def checkifempty():
+    if len(os.listdir(newfolder)) == 0:
+        os.remove(newfolder)
+
+
+movefilesintofolder()
+checkifempty()
