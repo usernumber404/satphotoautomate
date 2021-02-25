a407d70c8910f6fbf418066f15442150d9cc0dae
usernumber404 <72470454+usernumber404@users.noreply.github.com>
2021-02-25 10:38:55 -0700

    commit on main

--- satPhotoAutomation.py
+++ satPhotoAutomation.py
@@ -1,34 +1,35 @@
 import os
 import shutil
 import datetime
+import sys
 
 
 projectfolder = ''   #path to folder
 pathtodrive = ''   #path to drive DO NOT FORGET TO ADD AN / to the end
 currentdateandtime = datetime.datetime.now()
 currentdateandtimestring = str(currentdateandtime)
-perminatefolder = ''  #name of projections folder
-newfolder = currentdateandtimestring
+perminatefolder = 'projections'  #name of projections folder
+newfolder = pathtodrive + currentdateandtimestring
+
+
+def checkfolder():
+    if len(os.listdir(projectfolder)) > 2:
+        os.mkdir(newfolder)
+    else:
+        #print('exit')
+        sys.exit()
 
 
 def movefilesintofolder():
     os.chdir(projectfolder)
-    os.mkdir(newfolder)
-    for files in os.listdir():
+    for files in os.listdir(projectfolder):
         if files != perminatefolder:
             shutil.move(files, newfolder)
 
 
-def checkifemptythenmove():
-    if len(os.listdir(newfolder)) == 0:
-        os.rmdir(newfolder)
-    else:
-        shutil.move(newfolder, pathtodrive)
-
-
 def main():
+    checkfolder()
     movefilesintofolder()
-    checkifemptythenmove()
 
 
-main()
+
